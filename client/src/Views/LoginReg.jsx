a7ff4478afa9f73832f9f36246aa867944fac7a0
alex k <alex.kuhn1123@gmail.com>
2020-05-19 14:35:44 -0700

    add nav to LoginReg to test routing

--- client/src/Views/LoginReg.jsx
+++ client/src/Views/LoginReg.jsx
@@ -2,13 +2,20 @@ import React, { useState } from "react";
 
 import Login from '../components/Login';
 import Register from '../components/Register';
+import Nav from '../components/Nav';
 
 function LoginReg() {
     return (
+      <>
+      <div>
+        {/* adding nav bar in for development purposes to easily test routing */}
+        <Nav/>
+      </div>
       <div className="d-flex justify-content-center align-items-center">
         <Register/>
         <Login/>
       </div>
+      </>
     );
   }
   
