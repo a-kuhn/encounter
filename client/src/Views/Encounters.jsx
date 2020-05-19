7b114b73091c3f7bd0fb97f743b0728f4b297182
alex k <alex.kuhn1123@gmail.com>
2020-05-19 14:42:43 -0700

    add details to Encounters layout

--- client/src/Views/Encounters.jsx
+++ client/src/Views/Encounters.jsx
@@ -6,10 +6,15 @@ function NewChecklist() {
   return (
     <div className="container justify-content-center">
       <Nav />
+      <p>placeholder for user stats</p>
+      <ul>
+          <li>total # of species spotted</li>
+          <li>total # of places visited</li>
+      </ul>
       <p>placeholder for user's saved checklists</p>
       <ul>
         <li>Name of checklist (link to "/encounter/:checklistId")</li>
-        <li>location</li>
+        <li>location (link to live chat for location?? --> maybe this is a more of a bubble/chat window that pops up)</li>
         <li>date created</li>
         <li>last updated</li>
         <li>% complete</li>
